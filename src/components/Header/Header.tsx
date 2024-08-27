be8c9377c2dbf1b57a66a3ec099d8653a70978e6
박원빈 <harry21@kakao.com>
2024-08-27 22:21:55 +0900

    (design) Header: layout issue

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 flex justify-center w-screen bg-white z-50 border-b border-gray-500">
+    <header className="fixed top-0 left-0 flex justify-center w-full bg-white z-50 border-b border-gray-500">
       <div className="max-w-[1224px] w-full h-20 text-3xl font-bold flex justify-between items-center">
         <h1>nowCamp</h1>
         <div className="flex items-center gap-3 text-lg">
