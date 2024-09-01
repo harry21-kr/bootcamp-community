9748746662c6428ddd16b52150cde2c22383c64b
박원빈 <harry21@kakao.com>
2024-09-02 00:28:11 +0900

    (add) setup Providers

---
+++ src/components/Providers/QueryClientProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+const TanStackQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default TanStackQueryProvider;
