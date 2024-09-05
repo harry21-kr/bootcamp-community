5d833a2ec202efd5ba609d976ae9574d2fa94633
박원빈 <harry21@kakao.com>
2024-09-05 19:45:55 +0900

    (add) sign up page

---
+++ src/app/sign-up/layout.tsx
@@ -0,0 +1,7 @@
+const SignUpPageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">{children}</div>
+  );
+};
+
+export default SignUpPageLayout;
