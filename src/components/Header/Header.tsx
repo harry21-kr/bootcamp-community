5ba36646fedfa91fa1493c33a74d1395451e5a1b
박원빈 <harry21@kakao.com>
2024-09-13 21:23:32 +0900

    (add) display user nickname

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 const Header = async () => {
   const userData = await getUserData();
+  const nickname = userData.user?.user_metadata.nickname;
 
   return (
     <header className="fixed top-0 left-0 flex justify-center w-full bg-white z-50 border-b border-gray-500">
@@ -10,12 +11,15 @@ const Header = async () => {
         <h1>nowCamp</h1>
         <div className="flex items-center gap-3 text-lg">
           <button className="bg-black text-white px-5 py-2">글쓰기</button>
-          <Link
-            href={userData.user ? "/my" : "/login"}
-            className="border px-5 py-2"
-          >
-            {userData?.user?.email ?? "로그인"}
-          </Link>
+          {userData.user ? (
+            <Link href="/my" className="border px-5 py-2">
+              {nickname}
+            </Link>
+          ) : (
+            <Link href="/login" className="border px-5 py-2">
+              로그인
+            </Link>
+          )}
         </div>
       </div>
     </header>
